b3254f7ff9c9e0fa062d896266fdf27279a18874
kishan vasani <70906939+mrvasani48@users.noreply.github.com>
2021-04-06 19:27:49 +0530

    Add files via upload

---
+++ 63_defualt_constructor.js
@@ -0,0 +1,19 @@
+// default constructor
+class person{
+    clr=(clr)=>{
+        return clr;
+    };
+};
+var kishan= new person();
+console.log(kishan.clr("red"));
+
+//  default constructor
+ class person2 {
+     name="kishan d vasani";
+    clr=(clr)=>{
+        return clr;
+    };
+};
+var kishan=new person2();
+console.log(kishan.name);
+console.log(kishan.clr("gray"));
