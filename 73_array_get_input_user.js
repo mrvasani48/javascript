f2f514d22494082d3b3afa7fb31091e155323c63
kishan vasani <70906939+mrvasani48@users.noreply.github.com>
2021-04-07 17:28:37 +0530

    Add files via upload

---
+++ 73_array_get_input_user.js
@@ -0,0 +1,13 @@
+//get use total length of array
+var arr= prompt("input array length ",5);
+
+// declaration array
+var array=[];
+
+// intilizing array and  print array  value 
+for (let i=0;i<arr;i++ )
+{
+    array[i]=prompt("enter the array");
+    document.write(array[i]+" <br>");
+};
+
