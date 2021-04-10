a325a38e601e56fe2554e490f206d66fe368dc9b
kishan vasani <70906939+mrvasani48@users.noreply.github.com>
2021-04-10 19:29:46 +0530

    Add files via upload

---
+++ 82_string_interpolation_method.js
@@ -0,0 +1,19 @@
+var str = "my name is ";
+document.write(`${str} kishan <br> `);
+
+var str = "my name is ";
+document.write(`ohh this cool method ${str} kishan  <br>`);
+
+function myfunction (name)
+{
+   return name;
+}; 
+
+document.write(`my name is ${myfunction("kishan d vasani")}  <br>`);
+
+var array =["kishan","d","vasani"];
+document.write(`my name is ${array[0]} ${array[1]}  ${array[2]}<br>`);
+
+
+var obj= {fnmae:"kishan", mname:"d" ,lname:"vasani"};
+  document.write(`my name is ${obj.fnmae} ${obj.mname} `);
