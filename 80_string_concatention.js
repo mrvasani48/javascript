ebfa0bb273053921b0153b4307b5270568a76314
kishan vasani <70906939+mrvasani48@users.noreply.github.com>
2021-04-10 19:28:40 +0530

    Add files via upload

---
+++ 80_string_concatention.js
@@ -0,0 +1,7 @@
+var str1 = new String("this is string");
+var str2 =new String ('this is string');
+var str3 =new String( `this is string`);
+document.write(str1+"kishan vasani"+"<br>");
+document.write(str2.concat("kishan vasani"+"<br>"));
+document.write(str3.concat(str1));
+
