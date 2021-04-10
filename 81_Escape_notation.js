7ea3a5bc622fd3ec948dce6c6033554d4b893cdf
kishan vasani <70906939+mrvasani48@users.noreply.github.com>
2021-04-10 19:29:10 +0530

    Add files via upload

---
+++ 81_Escape_notation.js
@@ -0,0 +1,18 @@
+var str= "this is \n new line notation";
+console.log(str);
+var str= "this is \t tab notation";
+console.log(str);
+var str= "this is \0  notation";
+console.log(str);
+var str= "this is \\ notation";
+console.log(str);
+var str= "this is \' notation";
+console.log(str);
+var str= "this is \" notation";
+console.log(str);
+var str= "this is \` notation";
+console.log(str);
+var str= "this is \b notation";
+console.log(str);
+var str= "this is \f notation";
+console.log(str);
