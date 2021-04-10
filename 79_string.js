c666105a21d864582138b2031896d1f6284edadb
kishan vasani <70906939+mrvasani48@users.noreply.github.com>
2021-04-10 19:27:58 +0530

    Add files via upload

---
+++ 79_string.js
@@ -0,0 +1,19 @@
+var str1 = "this is string";
+var str2 = 'this is string';
+var str3 = `this is string`;
+document.write(str1+"<br>");
+document.write(str2+"<br>");
+document.write(str3+"<br>");
+var str = `this is string 
+`;
+console.log(str);
+
+var str1 = new String("this is string");
+var str2 =new String ('this is string');
+var str3 =new String( `this is string`);
+document.write(str1+"<br>");
+document.write(str2+"<br>");
+document.write(str3+"<br>");
+document.write("this 'is' string"+"<br>");
+document.write('this "is" string'+"<br>");
+
