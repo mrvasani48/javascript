a6debf17ebd75a86abe3204000b471ac3decaf9b
kishan vasani <70906939+mrvasani48@users.noreply.github.com>
2021-04-12 19:29:07 +0530

    Add files via upload

---
+++ 92_indexof_function.js
@@ -0,0 +1,17 @@
+var str =`kishan d vasani`;
+document.write(str.indexOf(1)+"<br>");//return -1
+document.write(str.indexOf("d")+"<br>");//return 7
+
+var array=[`kisan`,`d`,`vasani`];
+document.write(array.indexOf(`d`));//return 1
+/*
+var obj={
+            name:"kishan d vasani",
+            no:180200107118,
+            clg:"gecr"      
+          };
+document.write(obj.indexOf());
+//not work in object
+*/
+
+//this function work in string as well as array
