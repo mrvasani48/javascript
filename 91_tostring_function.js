d038a32e7de71bdb74d1a9d671f001bb7d308ef3
kishan vasani <70906939+mrvasani48@users.noreply.github.com>
2021-04-12 19:28:44 +0530

    Add files via upload

---
+++ 91_tostring_function.js
@@ -0,0 +1,9 @@
+var array=[`kisan`,`d`,`vasani`];
+var tostr=array.toString();
+document.write(tostr);
+//its convert  array to sttring or we can use join method
+
+var str=`kishan  d vasani`;
+var array=str.split(" ");
+document.write(array);
+//its convert  string to array
