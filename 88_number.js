a84019cfdf7a1f86aea49ef3c6b5b1a1dca302b6
kishan vasani <70906939+mrvasani48@users.noreply.github.com>
2021-04-12 10:07:29 +0530

    Add files via upload

---
+++ 88_number.js
@@ -0,0 +1,31 @@
+var a=1;
+var b="12"
+var c=a/b;
+document.write("a = "+a+"\t type of = "+typeof(a)+"<br>");//a = 1 type of = number
+document.write("b = "+b+" type of =  "+typeof(b)+"<br>");//b = 12 type of = string
+document.write("c = "+c+"  type of = " +typeof(c)+"<br>");//c = 0.0833 type of = number
+
+
+//number with string
+
+var a=12;
+var b=12;
+var c="kisha n" ;
+var d =45;
+document.write(" a = "+ a +"<br>");
+document.write(" b = "+ b +"<br>");
+document.write(" c = "+ c +"<br>");
+document.write(" d = "+ d +"<br>");
+
+document.write(" a + b =  ")
+document.write(a+b+"<br>")  //24
+document.write(" b + c =  ")
+document.write(b+c+"<br>")
+document.write(" a + c =  ")
+document.write(a+c+"<br>")
+document.write("a + b + c =  ")
+document.write(a+b+c+"<br>")
+document.write("a + b + c + d =  ")
+document.write(a+b+c+d+"<br>")
+document.write("a + b + d + c =  ")
+document.write(a+b+d+c+"<br>");
