116b5e8ae38a5e0e369aa63f731b205dc2aa151c
kishan vasani <70906939+mrvasani48@users.noreply.github.com>
2021-04-12 10:07:53 +0530

    Add files via upload

---
+++ 89_NAN.js
@@ -0,0 +1,30 @@
+a=12;
+b="string";
+document.write(a/b +"<br> ");
+
+//nan not eqaule to anyone
+/*// this code not working
+if (b==NAN)
+{
+    document.write("b is note number");
+}
+else
+{
+    document.write(" b is number");
+} 
+*/
+
+a=12;
+c="12"
+b="string";
+//document.write(isNaN(a)+"<br>");
+//document.write(isNaN(b));
+
+if (isNaN(c))
+{
+    document.write("b is not  number");
+}
+else
+{
+    document.write(" b is number");
+}
