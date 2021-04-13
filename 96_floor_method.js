c807abb5cd799251849c26a0e937a2129cb22e6e
kishan vasani <70906939+mrvasani48@users.noreply.github.com>
2021-04-13 15:16:23 +0530

    Add files via upload

---
+++ 96_floor_method.js
@@ -0,0 +1,7 @@
+var a=123.45647829;
+document.write(Math.floor(a)+"<br>");//123
+document.write(Math.floor(12.65)+"<br>");//12
+document.write(Math.floor(19.9)+"<br>");//19
+document.write(Math.floor(-4.5)+"<br>");//-5
+document.write(Math.floor(-4.6)+"<br>");//-5
+document.write(Math.floor(-4.1)+"<br>");//-5
