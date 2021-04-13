d84789e85bcbedb26823c4aea154be8d3ebfdc05
kishan vasani <70906939+mrvasani48@users.noreply.github.com>
2021-04-13 23:37:42 +0530

    Add files via upload

---
+++ 101_set_method.js
@@ -0,0 +1,27 @@
+var day = new Date();
+day.setFullYear(3020,11,25);
+document.write(day+"<br>");
+
+day.setYear(1010);
+document.write(day+"<br>");
+
+day.setMonth(10);
+document.write(day+"<br>");
+
+day.setDate(5);
+document.write(day+"<br>");
+
+day.setHours(5);
+document.write(day+"<br>");
+
+day.setMinutes(05);
+document.write(day+"<br>");
+
+day.setSeconds(65);
+document.write(day+"<br>");
+
+day.setFullYear(3050);
+document.write(day+"<br>"+"<br>");
+
+day.setUTCDate(0002,01,02);
+document.write(day+"<br>");
