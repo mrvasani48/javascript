ec2323235c53c5e2c53db996f434ca43ee59f4f9
kishan vasani <70906939+mrvasani48@users.noreply.github.com>
2021-04-13 23:39:48 +0530

    Add files via upload

---
+++ 102_get_method.js
@@ -0,0 +1,19 @@
+var day = new Date();
+document.write(day+"<br>");//Tue Apr 13 2021 19:25:36 GMT+0530 (India Standard Time)
+
+document.write(day.getFullYear()+"<br>");
+//2021
+document.write(day.getMonth()+"<br>");
+//3-april
+document.write(day.getDay()+"<br>");
+//2-tuesday
+document.write(day.getHours()+"<br>");
+//19
+document.write(day.getMinutes()+"<br>");
+//25
+document.write(day.getSeconds()+"<br>");
+//36
+document.write(day.getUTCFullYear()+"<br>");
+//2021
+document.write(day.getUTCMonth()+"<br>");
+//3
