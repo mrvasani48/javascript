86c6621af61588214754616bb059f9374c85784f
kishan vasani <70906939+mrvasani48@users.noreply.github.com>
2021-04-13 15:16:40 +0530

    Add files via upload

---
+++ 97_round_method.js
@@ -0,0 +1,7 @@
+var a=123.45647829;
+document.write(Math.round(a)+"<br>");//123
+document.write(Math.round(12.65)+"<br>");//13
+document.write(Math.round(19.9)+"<br>");//20
+document.write(Math.round(-4.5)+"<br>");//-4
+document.write(Math.round(-4.6)+"<br>");//-5
+document.write(Math.round(-4.1)+"<br>");//-4
