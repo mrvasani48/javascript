3c68cdee8d5148356024bc4f5e3bea4066531d53
kishan vasani <70906939+mrvasani48@users.noreply.github.com>
2021-04-13 15:18:48 +0530

    Add files via upload

---
+++ 98_random_method_generate_number.js
@@ -0,0 +1,5 @@
+var a=Math.random();
+document.write("random int 1 to 10= "+Math.floor(a*10+1)+"<br>");
+document.write("random int 1 to 100= "+Math.round(a*100+1)+"<br>");
+document.write("random int 1 to 1000= "+Math.floor(a*1000+1)+"<br>");
+document.write("random int 1 to 10000= "+Math.round(a*10000+1)+"<br>");
