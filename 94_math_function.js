ee984ae65f010d6d558f5cc86c8531e1a4229e07
kishan vasani <70906939+mrvasani48@users.noreply.github.com>
2021-04-13 13:48:10 +0530

    Add files via upload

---
+++ 94_math_function.js
@@ -0,0 +1,30 @@
+document.write(Math.PI+"<br>");
+
+document.write(`${Math.log(10)}<br>`);//3.141592653589793
+document.write(`${Math.log10(10)}<br>`);//2.302585092994046
+document.write(`${Math.log1p(10)}<br>`);//1
+document.write(`${Math.log2(10)}<br>`);//2.3978952727983707
+
+document.write(`${Math.LOG10E}<br>`);//3.321928094887362
+document.write(`${Math.LOG2E}<br>`);//0.4342944819032518
+document.write(`${Math.E}<br>`);//2.718281828459045
+
+document.write(`${Math.SQRT1_2}<br>`);//0.7071067811865476  hyperbolic  arccosine of arg
+document.write(`${Math.SQRT2}<br>`);//1.4142135623730951
+
+
+document.write(`${Math.abs(-12.12)}<br>`);//12.12  e absulate value
+
+document.write(`${Math.acos(1)}<br>`);//0  arccosine of arg
+document.write(`${Math.acosh(30)}<br>`);//4.0940666686320855  hyperbolic cosine
+
+document.write(`${Math.asin(0)}<br>`);//0
+document.write(`${Math.asinh(30)}<br>`);//4.09462222433053
+
+document.write(`${Math.atan(1)}<br>`);//0.7853981633974483
+document.write(`${Math.atan2(1,1)}<br>`);//0.7853981633974483
+document.write(`${Math.atanh(0)}<br>`);//0
+
+document.write(Math.sqrt(64)+"</BR>");
+document.write(`${Math.pow(2,3)}<br>`);//0
+document.write(`${Math.trunc(13.64)}<br>`);//0
