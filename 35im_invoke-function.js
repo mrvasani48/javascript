ef330d9dd8ba1d67fdfaa0cdc9eecdd4776a6835
kishan vasani <70906939+mrvasani48@users.noreply.github.com>
2021-05-24 16:24:10 +0530

    Update 35im_invoke-function.js

--- 35im_invoke-function.js
+++ 35im_invoke-function.js
@@ -1,5 +1,10 @@
-( function(f,g){document.write(f+g+"<br>"); }) (11,12);
+( function(f,g){
+document.write(f+g+"<br>");
+ })
+ (11,12);
+
 ((f,g)=>{
+
  	document.write(f+g+"<br>")})(45,45);
 (function myfuction(f,g){
  	document.write(f+g+"<br>")})(90,90);
